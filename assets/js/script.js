ef51c3d4fdd495268c4e296377bb5ae2ca722881
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-09-08 10:13:53 +0100

    Add active namaaz code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+let ctime = document.getElementById("fullTime");
+let cSec = document.getElementById("seconds");
+let cAMPM = document.getElementById("ampm");
+let cDay = document.getElementById("day");
+let cDate = document.getElementById("date");
+
+cDay.innerHTML = writeDay();
+cDate.innerHTML = writeDate();
+
 document.addEventListener("DOMContentLoaded", function() {
     let main = document.querySelector('main');
     main.addEventListener('click', function() {
@@ -36,6 +45,52 @@ function inputUpdate(id, input) {
     localStorage.setItem(id, input)
 }
 
+function getActiveNamaaz() {
+    let activeNamaaz = document.getElementsByClassName('active')[0];
+    return activeNamaaz;
+}
+function checkTimer(namaaz, date) {
+    let currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+    let namaazTime
+    if (namaaz.getAttribute('id') == 'fajr' || namaaz.getAttribute('id') == 'eid') {
+        namaazTime = namaaz.lastElementChild.innerText;
+    }
+    else{
+        namaazTime = add12Hours(namaaz.lastElementChild.innerText)
+    }
+    let timer = returnTimeDifference(currentTime , namaazTime)
+    if (timer < 0) {
+        namaaz.classList.remove('active');
+    }
+    //console.log(timer)
+}
+
+function add12Hours(timeParameter) {
+    let arrayTime = timeParameter.split(":");
+    if (Number(arrayTime[0]) < 12) {
+        arrayTime[0] += 12;
+    }
+    return arrayTime[0] + ':' + arrayTime[1];
+}
+
+function convertToSeconds(timeParameter) {
+    let arrayTime = timeParameter.split(":");
+    arrayTime.forEach(mySecondsFunction);
+    let sum = arrayTime.reduce((partialSum, a) => partialSum + a, 0)
+    return sum
+}
+
+function mySecondsFunction(item, index, arr) {
+    arr[index] = item * 3600 / (60 ** index);
+    
+}
+function returnTimeDifference(currentTime, namaazTime) {
+    let cTSec = convertToSeconds(currentTime);
+    let nTSec = convertToSeconds(namaazTime)
+    let result = nTSec - cTSec;
+    return result
+}
+
 function formatTIME12H(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes();
@@ -64,22 +119,18 @@ function writeDay() {
 	return outputDay;
 }
 
-let ctime = document.getElementById("fullTime");
-let cSec = document.getElementById("seconds");
-let cAMPM = document.getElementById("ampm");
-let cDay = document.getElementById("day");
-let cDate = document.getElementById("date");
-
-cDay.innerHTML = writeDay();
-cDate.innerHTML = writeDate();
-
 setInterval(() => {
     let d = new Date();
     let dHours = d.getHours();
+    let dMinutes = d.getMinutes();
     let dSec = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
     let ampm = dHours >= 12 ? 'pm' : 'am';
+    let activeNamaaz = getActiveNamaaz();
 
     ctime.innerHTML = formatTIME12H(d);
+    checkTimer(activeNamaaz, d)
     cSec.innerHTML = dSec;
     cAMPM.innerHTML = ampm;
+    if (dHours == 0 && dMinutes == 0 && dSec == 0) { location.reload(); }
+
 }, 1000)
