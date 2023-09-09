fb3dd2c3fde4a14dc10d3b7c65b3eab8307ab43a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-09-09 07:56:03 +0100

    Add localstorage variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,19 @@ let cSec = document.getElementById("seconds");
 let cAMPM = document.getElementById("ampm");
 let cDay = document.getElementById("day");
 let cDate = document.getElementById("date");
+let cIDate = document.getElementById("hijri");
+let eidTime = document.getElementById("eidJamaat");
+let message = document.getElementById("announcements");
+
+const adjustIDate = localStorage.getItem('inputAdjustIDate');
+const eidNamaazTime = localStorage.getItem('inputEidTime');
+const announcements = localStorage.getItem('inputAnnouncements');
 
 cDay.innerHTML = writeDay();
 cDate.innerHTML = writeDate();
+cIDate.innerHTML = writeIslamicDate(adjustIDate);
+eidTime.innerHTML = eidNamaazTime;
+message.innerHTML += announcements;
 
 document.addEventListener("DOMContentLoaded", function() {
     let main = document.querySelector('main');
@@ -39,7 +49,104 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+function gmod(n,m){
+	return ((n%m)+m)%m;
+}
 
+function kuwaiticalendar(adjust){
+	let today = new Date();
+	if(adjust) {
+		adjustmili = 1000*60*60*24*adjust; 
+		todaymili = today.getTime()+adjustmili;
+		today = new Date(todaymili);
+	}
+	day = today.getDate();
+	month = today.getMonth();
+	year = today.getFullYear();
+	m = month+1;
+	y = year;
+	if(m<3) {
+		y -= 1;
+		m += 12;
+	}
+
+	a = Math.floor(y/100.);
+	b = 2-a+Math.floor(a/4.);
+	if(y<1583) b = 0;
+	if(y==1582) {
+		if(m>10)  b = -10;
+		if(m==10) {
+			b = 0;
+			if(day>4) b = -10;
+		}
+	}
+
+	jd = Math.floor(365.25*(y+4716))+Math.floor(30.6001*(m+1))+day+b-1524;
+
+	b = 0;
+	if(jd>2299160){
+		a = Math.floor((jd-1867216.25)/36524.25);
+		b = 1+a-Math.floor(a/4.);
+	}
+	bb = jd+b+1524;
+	cc = Math.floor((bb-122.1)/365.25);
+	dd = Math.floor(365.25*cc);
+	ee = Math.floor((bb-dd)/30.6001);
+	day =(bb-dd)-Math.floor(30.6001*ee);
+	month = ee-1;
+	if(ee>13) {
+		cc += 1;
+		month = ee-13;
+	}
+	year = cc-4716;
+
+	if(adjust) {
+		wd = gmod(jd+1-adjust,7)+1;
+	} else {
+		wd = gmod(jd+1,7)+1;
+	}
+
+	iyear = 10631./30.;
+	epochastro = 1948084;
+	epochcivil = 1948085;
+
+	shift1 = 8.01/60.;
+	
+	z = jd-epochastro;
+	cyc = Math.floor(z/10631.);
+	z = z-10631*cyc;
+	j = Math.floor((z-shift1)/iyear);
+	iy = 30*cyc+j;
+	z = z-Math.floor(j*iyear+shift1);
+	im = Math.floor((z+28.5001)/29.5);
+	if(im==13) im = 12;
+	id = z-Math.floor(29.5001*im-29);
+
+	var myRes = new Array(8);
+
+	myRes[0] = day; //calculated day (CE)
+	myRes[1] = month-1; //calculated month (CE)
+	myRes[2] = year; //calculated year (CE)
+	myRes[3] = jd-1; //julian day number
+	myRes[4] = wd-1; //weekday number
+	myRes[5] = id; //islamic date
+	myRes[6] = im-1; //islamic month
+	myRes[7] = iy; //islamic year
+
+	return myRes;
+}
+function writeIslamicDate(adjustment) {
+	//let wdNames = new Array("Ahad","Ithnin","Thulatha","Arbaa","Khams","Jumuah","Sabt");
+	let iMonthNames = new Array("Muharram","Safar","Rabi'ul Awwal","Rabi'ul Akhir",
+	"Jumadal Awwal","Jumadal Akhir","Rajab","Sha'ban",
+	"Ramadan","Shawwal","Dhul Qa'ada","Dhul Hijja");
+	let iDate = kuwaiticalendar(adjustment);
+	//let dateID = adjustID + iDate[5];
+    let dateID = iDate[5];
+	//let outputIslamicDate = wdNames[iDate[4]] + ", " + iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
+	let outputIslamicDate = dateID + " " + iMonthNames[iDate[6]] + " " + iDate[7];
+	return outputIslamicDate;
+}
 
 function inputUpdate(id, input) {
     localStorage.setItem(id, input)
@@ -51,12 +158,12 @@ function getActiveNamaaz() {
 }
 function checkTimer(namaaz, date) {
     let currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-    let namaazTime
+    let namaazTime;
     if (namaaz.getAttribute('id') == 'fajr' || namaaz.getAttribute('id') == 'eid') {
-        namaazTime = namaaz.lastElementChild.innerText;
+        namaazTime = namaaz.getAttribute('data-type');
     }
     else{
-        namaazTime = add12Hours(namaaz.lastElementChild.innerText)
+        namaazTime = add12Hours(namaaz.getAttribute('data-type'))
     }
     let timer = returnTimeDifference(currentTime , namaazTime)
     if (timer < 0) {
